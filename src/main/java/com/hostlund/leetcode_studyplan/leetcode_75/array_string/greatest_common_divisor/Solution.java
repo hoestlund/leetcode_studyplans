9db41e8bf48552391dff5dc37e8e2e3130cab51f
Oskar Höstlund <oskarhostlund@gmail.com>
2025-09-27 14:08:03 +0200

    Confused by this, skipping

---
+++ src/main/java/com/hostlund/leetcode_studyplan/leetcode_75/array_string/greatest_common_divisor/Solution.java
@@ -0,0 +1,10 @@
+package com.hostlund.leetcode_studyplan.leetcode_75.array_string.greatest_common_divisor;
+
+public class Solution {
+
+  //TODO
+  public String gcdOfStrings(String str1, String str2) {
+    return "";
+  }
+
+}
