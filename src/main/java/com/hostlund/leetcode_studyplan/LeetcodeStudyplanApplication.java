3da4d6129631e562166222351398b53bfab6b353
Oskar Höstlund <oskarhostlund@gmail.com>
2025-09-27 13:50:53 +0200

    init commit

---
+++ src/main/java/com/hostlund/leetcode_studyplan/LeetcodeStudyplanApplication.java
@@ -0,0 +1,13 @@
+package com.hostlund.leetcode_studyplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeStudyplanApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LeetcodeStudyplanApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hostlund/leetcode_studyplan/LeetcodeStudyplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostlund.leetcode_studyplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeStudyplanApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
